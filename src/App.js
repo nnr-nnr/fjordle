fe80f3d04d5b1debe37f3d4f6c6a3862f46e7fa2
Niva Razin <nivarazin@Nivas-MacBook-Pro.local>
2022-09-26 22:16:52 -0700

    removing maps stuff

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import "./App.css";
 import Grid from "./components/Grid";
 import Keyboard from "./components/Keyboard";
-// import Map from "./components/Map";
 import { GuessProvider } from "./utils/Context";
-import MyGlobe from "./components/Globe";
 import Navbar from "./components/Navbar";
 
 function App() {
@@ -11,10 +9,7 @@ function App() {
     <GuessProvider>
       <div className="App">
         <Navbar />
-
-        {/* <Map /> */}
         <Grid />
-        <MyGlobe />
         <Keyboard />
       </div>
     </GuessProvider>
